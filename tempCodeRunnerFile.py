346b6205f390e52d4984cf06acaf27244a4c5738
Faiz <hasanfaiz2107@gmail.com>
2025-11-05 13:36:56 +0530

    Remove temporary file

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-nr_letters=int(input("How many letters would you like in your password?\n"))
-# nr_symbols=int(input("How many symbols would you like in your password?\n"))
-# nr_numbers=int(input("How many numbers would you like in your password?\n"))
